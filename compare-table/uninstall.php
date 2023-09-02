d90c4a0de8d294b11a439865fd765d9eb558e8c9
joerivanveen <nonstockphoto@gmail.com>
2023-09-02 09:08:51 +0200

    style: format for wordpress code style

--- compare-table/uninstall.php
+++ compare-table/uninstall.php
@@ -1,13 +1,13 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types=1 );
 
 // if uninstall.php is not called by WordPress, die
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-    die();
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die();
 }
 
-include_once('compare-table.php');
+include_once( 'compare-table.php' );
 
 global $ruigehond014;
 $ruigehond014->uninstall();
